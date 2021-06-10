4217feb0b32f35a1ccd371fdeb8827d3d39f0aac
AsmaSimreen <asmasimreen1001@gmail.com>
2021-06-10 08:05:36 +0530

    Functional DashBoard

--- components/src/components/Dashboard.js
+++ components/src/components/Dashboard.js
@@ -1,92 +1,117 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import "./css/Dashboard.css";
 import axios from "axios";
 import Loading from "../images/loading.gif";
-export default class Dashboard extends Component {
-  state = {
-    query: "",
-    users: null,
-    loading: true,
-  };
+export default function Dashboard() {
+  const [query, setQuery] = useState("");
+  const [error, setError] = useState(false);
+  const [users, setUsers] = useState(null);
+  const [loading, setloading] = useState(true);
 
-  componentDidMount() {
+
+  // export default class Dashboard extends Component {
+  //   state = {
+  //     query: "",
+  //     users: null,
+  //     loading: true,
+  //   };
+  useEffect(() => {
     const getUsers = async () => {
       try {
         const res = await axios.get("https://api.github.com/users");
-        this.setState({ users: res.data, loading: false });
+        setUsers(res.data);
+        setloading(false);
+
       } catch (err) {
         console.log(err);
       }
     };
     getUsers();
-  }
-  componentWillUnmount() {
-    this.setState({ users: null });
-  }
-  handleSearch = async (ev) => {
+    return () => {
+      setUsers(null);
+    };
+  }, []);
+
+  //   componentDidMount() {
+  //     const getUsers = async () => {
+  //       try {
+  //         const res = await axios.get("https://api.github.com/users");
+  //         this.setState({ users: res.data, loading: false });
+  //       } catch (err) {
+  //         console.log(err);
+  //       }
+  //     };
+  //     getUsers();
+  //   }
+  //   componentWillUnmount() {
+  //     this.setState({ users: null });
+  //   }
+  const handleSearch = async (ev) => {
     // implementation of handleSearch using
     // https://api.github.com/search/users?q=pranis
     ev.preventDefault();
     try {
-      if (this.state.query === "") {
-        this.setState({ error: true });
+      if (query === "") {
+        setError(true);
         setTimeout(() => {
-          this.setState({ error: false });
-        })
+          setError(false);
+        }, 5000);
         return;
       }
-      this.setState({ loading: true });
+      setloading(true);
       const res = await axios.get(
         `https://api.github.com/search/users?q=${this.state.query}`
       );
 
-      this.setState({ users: res.data.items, loading: false });
+      setUsers(res.data.items);
+      setloading(false);
+
     } catch {
       console.log("Something went wrong");
     }
   };
-  render() {
-    return (
-      <>
-        <h2>Dashboard</h2>
-        <div className="searchBar">
-          <form onSubmit={this.handleSearch}>
-            <input
-              className={
-                this.state.error ? "search_input input_error" : "search_input"
-              }
-              value={this.state.query}
-              onChange={(ev) => {
-                this.setState({ query: ev.target.value });
-              }}
-              type="text"
-              className="search_input"
-              placeholder="Enter a keyword to search..."
-            ></input>
-          </form>
-        </div>
-        <div className="dashboardContainer">
-          {this.state.loading ? (
-            <img className="loadingImage" src={Loading} alt="Loading" />
-          ) : (
-            this.state.users
-              ?.filter((user) => {
-                return user.login.startsWith(this.state.query);
-              })
-              .map((user) => {
-                return (
-                  <Card
-                    avatar_url={user.avatar_url}
-                    login={user.login}
-                    github_link={user.html_url}
-                  />
-                );
-              })
-          )}
-        </div>
-      </>
-    );
-  }
+
+  return (
+    <>
+      <h2>Dashboard</h2>
+      <div className="searchBar">
+        <form onSubmit={handleSearch}>
+          <input
+            className={
+              error ? "search_input input_error" : "search_input"
+            }
+            value={query}
+            onChange={(ev) => {
+              setQuery(ev.target.value);
+              // this.setState({ query: ev.target.value });
+            }}
+            type="text"
+            className="search_input"
+            placeholder="Enter a keyword to search..."
+          ></input>
+        </form>
+      </div>
+      <div className="dashboardContainer">
+        {loading ? (
+          <img className="loadingImage" src={Loading} alt="Loading" />
+        ) : (
+          users
+            ?.filter((user) => {
+              return user.login.startsWith(query);
+            })
+            .map((user) => {
+              return (
+                <Card
+                  avatar_url={user.avatar_url}
+                  login={user.login}
+                  github_link={user.html_url}
+                />
+              );
+            })
+        )}
+      </div>
+    </>
+  );
 }
 // Dashboard.propTypes = { users: PropTypes.object.isRequired };
